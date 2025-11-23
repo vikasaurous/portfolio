4c09309b51d57f35acdc583c893c31799dad0f98
Vikas Yadav <vikas.yadav.connect@gmail.com>
2025-11-23 17:23:20 +0530

    completed navbar design with dynamic clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Navbar from '#components/Navbar'
 import React from 'react'
 
 const App = () => {
-  return (
-    <div>
-      <h1 className='font-extrabold underline'>Welcome to my macfolio </h1>
-    </div>
-  )
+  return <main>
+    <Navbar />
+  </main>
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "#constants";
+import { useEffect, useState } from "react";
+
+const Navbar = () => {
+  const [currentTime, setCurrentTime] = useState(dayjs());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(dayjs());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formattedTime = currentTime.format("ddd MMM D h:mm:ss A");
+
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="apple logo" />
+        <p className="font-bold">Vikas's Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+
+        <time className="tabular-nums">{formattedTime}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
